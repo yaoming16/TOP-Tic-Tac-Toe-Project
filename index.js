84c5162fc9086255f9ec62127d350a5856ef90ba
Pablo Perez <pabloperezzz16@gmail.com>
2025-10-24 21:19:11 +0000

    Basic logic added. Right now you can play and see the plays on screen. Need to add win condition an buttons to reset and add player names

---
+++ index.js
@@ -0,0 +1,69 @@
+// Constructor of the player object
+function createPlayer (name, mark) {
+    const getName = () => name;
+    const getMark = () => mark;
+    return {getName, getMark};
+} 
+
+// COnstructor for the game board
+function createGameBoard () {
+    const board = ["", "", "", "", "", "", "", "", ""];
+    const getBoard = () => board;
+
+    const addToBoard = (index, playerMark) =>  {
+        if (index < 9 && index >= 0) {
+            board[index] = playerMark;
+        }
+    }
+
+    const resetGameBoard = (gameBoxes) => {
+        board.map((element) => element = "");
+        gameBoxes.map((element) => element.innerHTML = "");
+    }
+    
+    const boardHasEmptySpace = () => board.some((value) => value === "");
+    return {getBoard, addToBoard, boardHasEmptySpace, resetGameBoard};
+}
+
+// Game object
+const game = ( function () {
+    const player1 = createPlayer("Player 1", "X");
+    const player2 = createPlayer("Player 2", "O");
+    const {getBoard, addToBoard, boardHasEmptySpace} = createGameBoard(); 
+
+    let currentPlayer = player1;
+    const getCurrentPlayer = () => currentPlayer;
+
+    const makePlay = (index, element) => {
+        // Only add the play if the selected space is empty
+        if (getBoard()[index] === "") {
+            addToBoard(index, currentPlayer.getMark());
+            element.append(currentPlayer.getMark());
+            console.log(element);
+            currentPlayer = currentPlayer === player1 ? player2 : player1; 
+            console.log(game.getBoard());
+
+        }
+    }
+    
+    return {getCurrentPlayer, makePlay, getBoard, boardHasEmptySpace}
+
+})();
+
+const gameDiv = document.querySelector("#game-div");
+const gameBoxes = document.querySelectorAll(".game-box");
+
+// Add event listener to the game box to add the play to the board
+document.querySelectorAll('.game-box').forEach((el, i) => {
+    el.addEventListener('click', () => game.makePlay(el.id, el))
+} )
+
+
+
+
+
+
+
+
+
+
